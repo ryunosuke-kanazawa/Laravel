6af53e73d5af4a15983f9d83fb82ddeda6760fef
ryunosuke-kanazawa <ryunosuke0222@icloud.com>
2022-03-10 12:40:49 +0900

    3/10

--- laravelapp/app/Http/Controllers/HelloController.php
+++ laravelapp/app/Http/Controllers/HelloController.php
@@ -3,12 +3,31 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\HelloRequest;
+use Validator;
 
 class HelloController extends Controller
 {
 
     public function index(Request $reqest)
     {
-        return view('hello.index');
+        $rules = [
+            'name' => 'required',
+            'mail' => 'email',
+            'age' => 'numeric|between:0,150',
+        ];
+        $massages = [
+            'name.required' => '名前は必ず入力してください。',
+            'mail.email' => 'メールアドレスが必要です。',
+            'age.numeric' => '年齢を整数で入力してください。',
+            'age.between' => '年齢は0～150の間で入力してください。',
+        ];
+        $validator = Validator::make($request->all(), $rules, $massages); 
+        if ($validator->fails()){
+            return redirect('/hello')
+            ->withErrors($validator)
+            ->withInput();
+        }
+        return view('hello.index', ['msg'=>'正しく入力されました。']);
     }
 }
--- laravelapp/resources/views/hello/index.blade.php
+++ laravelapp/resources/views/hello/index.blade.php
@@ -8,10 +8,17 @@
 @endsection
 
 @section('content')
-    <p>ここが本文のコンテンツです。</p>
-    <p>これは、<middleware>google.com</middleware>へのリンクです。</p>
-    <p>これは、<middleware>yahoo.co.jp</middleware>へのリンクです。</p>
-@endsection
+    <p>{{$msg}}</p>
+    @if (count($errors) > 0)
+    <p>入力に問題があります。再入力してください。</p>
+    @endif
+    <form action="/hallo" method="post">
+        <table>
+            @csrf
+            @error('name')
+            <tr><th>ERROR</th>
+            <td>{{$message}}</td></tr>
+            @enderror
 
 @sectiion('footer')
     copyright 2020 tuyano.
